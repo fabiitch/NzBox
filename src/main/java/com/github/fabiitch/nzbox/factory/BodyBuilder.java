91fe53366cfa48c9c15e953071305e96c82d2b12
fabien occelli <fabien.occelli@hidglobal.com>
2024-09-12 17:04:27 +0200

    factory todo

---
+++ src/main/java/com/github/fabiitch/nzbox/factory/BodyBuilder.java
@@ -0,0 +1,15 @@
+package com.github.fabiitch.nzbox.factory;
+
+import com.badlogic.gdx.math.Vector2;
+import com.github.fabiitch.nzbox.bodies.BodyType;
+
+public class BodyBuilder {
+
+    private Vector2 position;
+    private Vector2 velocity;
+    private BodyType bodyType;
+
+
+
+
+}
---
+++ src/main/java/com/github/fabiitch/nzbox/factory/BoxFactory.java
@@ -0,0 +1,4 @@
+package com.github.fabiitch.nzbox.factory;
+
+public class BoxFactory {
+}
